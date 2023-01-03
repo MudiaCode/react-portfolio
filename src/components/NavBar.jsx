60c8e529ab95e1198379b06ab2831d727f500c1f
MudiaUON <mudiauon@gmail.com>
2023-01-03 17:55:20 +0000

    Started responsive hamburger menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,8 +12,12 @@ function NavBar() {
             className="  h-14 sm:h-12 xs:h-12 xxs:h-12 drop-shadow-lg absolute hover:scale-90 transform transition duration-500"
           />
         </a>
-
-        <ul className=" flex space-x-16 text-xs  my-5  font-thin text-white md:text-[0.7rem] sm:text-[0.6rem] sm:invisible xs:invisible xxs:invisible" >
+        <button className="text-cyan-400 border-cyan-400 lg:hidden md:hidden xl:hidden p-1.5 rounded space-y-2 mt-2">
+          <div className="w-4 h-1 bg-cyan-400 rounded "></div>
+          <div className="w-7 h-1 bg-cyan-400 rounded"></div>
+          <div className="w-7 h-1 bg-cyan-400 rounded"></div>
+        </button>
+        <ul className=" flex space-x-16 text-xs  my-5  font-thin text-white md:text-[0.7rem] sm:text-[0.6rem] sm:hidden xs:hidden xxs:hidden">
           <li>
             <a
               className="hover:text-cyan-400 transform transition duration-500"
